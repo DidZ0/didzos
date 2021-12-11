c2edc8127f1d8024adcf89138ae9d9a6a0a764c7
Yan IMENSAR <sub@yan-imensar.fr>
2021-12-11 02:36:30 +0100

    :wrench: updated structure

--- config/install.sh
+++ config/install.sh
@@ -4,5 +4,7 @@ path=$(pwd)
 
 # INSTALL DWM
 rm -rf $path/dwm/out/*
+cp $path/dwm/flexipatch/patches.def.h $path/dwm/flexipatch/patches.h
 $path/tools/flexipatch-finalizer.sh -r -d $path/dwm/flexipatch -o $path/dwm/out
+rm $path/dwm/flexipatch/patches.h
 sudo make -C $path/dwm/out clean install
