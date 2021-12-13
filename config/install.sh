a8129a9b3dc477ec55c4a18132f9189635ac79ae
Yan IMENSAR <sub@yan-imensar.fr>
2021-12-13 11:32:18 +0100

    :sparkles: moving to a cleaner approach

--- config/install.sh
+++ config/install.sh
@@ -1,23 +1,12 @@
 #! /bin/sh
 
 path=$(pwd)
-dwm_path=$path/dwm
-dmenu_path=$path/dmenu
-st_path=$path/st
 
-# INSTALL DWM
-rm -rf $dwm_path/out/*
-cp $dwm_path/flexipatch/patches.def.h $dwm_path/flexipatch/patches.h
-$path/tools/flexipatch-finalizer.sh -r -d $dwm_path/flexipatch -o $dwm_path/out
-rm $dwm_path/flexipatch/patches.h
-sudo make -C $dwm_path/out clean install
+sudo make -C $path/dwm/ clean install
+rm -f $path/dwm/config.h
 
-# INSTALL DMENU
-# TODO
+sudo make -C $path/dmenu/ clean install
+rm -f $path/dmenu/config.h
 
-# INSTALL ST 
-rm -rf $st_path/out/*
-cp $st_path/flexipatch/patches.def.h $st_path/flexipatch/patches.h
-$path/tools/flexipatch-finalizer.sh -r -d $st_path/flexipatch -o $st_path/out
-rm $st_path/flexipatch/patches.h
-sudo make -C $st_path/out clean install
+sudo make -C $path/st/ clean install
+rm -f $path/st/config.h
