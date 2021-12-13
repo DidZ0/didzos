4b1cf4957681524bd6d05e35d36a9cb75fd33cc4
Yan IMENSAR <sub@yan-imensar.fr>
2021-12-13 11:36:32 +0100

    :recycle: refact scripts

--- didzos.sh
+++ didzos.sh
@@ -1,36 +1,25 @@
 #!/bin/sh
 path=$(pwd)
 
-
-
 #DEPS
 paru -S --needed python nitrogen neovim picom-git python-pywal
 
-#HOME 
-ln -sf $path/.zshrc $HOME/.zshrc
-
-ln -sf $path/.xinitrc $HOME/.xinitrc
-
-#CONFIG
+#DELETE 
 [ ! -d $HOME/.config ] && mkdir $HOME/.config
-
 [ -d $HOME/.config/dwm ] && rm -rf $HOME/.config/dwm
-ln -sf $path/config/dwm $HOME/.config/dwm
-
 [ -d $HOME/.config/st ] && rm -rf $HOME/.config/st
-ln -sf $path/config/st $HOME/.config/st
-
 [ -d $HOME/.config/dmenu ] && rm -rf $HOME/.config/dmenu
-ln -sf $path/config/dmenu $HOME/.config/dmenu
-
 [ -d $HOME/.config/resources ] && rm -rf $HOME/.config/resources
-ln -sf $path/config/resources $HOME/.config/resources
-
-ln -sf $path/config/picom.conf $HOME/.config/picom.conf
-
-#CACHE
 [ ! -d $HOME/.cache ] && mkdir $HOME/.cache
+
+#LINK
+ln -sf $path/.xinitrc $HOME/.xinitrc
+ln -sf $path/config/resources $HOME/.config/resources
+ln -sf $path/config/picom.conf $HOME/.config/picom.conf
 ln -sf $path/cache/wal $HOME/.cache/wal
+ln -sf $path/config/dwm $HOME/.config/dwm
+ln -sf $path/config/st $HOME/.config/st
+ln -sf $path/config/dmenu $HOME/.config/dmenu
 
 #ETC
 sudo ln -sf $path/etc/trackpad/30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf
